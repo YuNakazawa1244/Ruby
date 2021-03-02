cc4d9df7ea9e1a023a68499b13cfe71ec5b8d0e1
YuNakazawa1244 <g_o_w_yu321903@yahoo.ne.jp>
2021-03-02 12:30:41 +0000

    [Finish]2-8

---
+++ greeting.rb
@@ -0,0 +1,16 @@
+###カリキュラム記載に誤りがあるのでは？
+###ご意見に投稿済。HelloとGood morningの間に改行が必要なのでは？
+
+
+def greeting(name)
+  return "Hello,#{name}!"
+  "Good morning,#{name}!"
+end
+
+puts greeting('John')
+
+# def greeting(name)
+#   return "Hello, #{name}!"  "Good morning, #{name}!"
+# end
+
+# puts greeting('John')
